b971c60c7806877173406e761cfccf66e2de4dd2
Saikot hasan <49225310+saikothasan@users.noreply.github.com>
2025-02-10 21:29:09 +0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,56 +1,18 @@
-import "./globals.css"
-import { Inter } from "next/font/google"
-import Header from "../components/Header"
-import Footer from "../components/Footer"
-import { ThemeProvider } from "../components/ThemeProvider"
-import type { Metadata } from "next"
 import type { ReactNode } from "react"
+import Header from "./Header"
+import Footer from "./Footer"
 
-const inter = Inter({ subsets: ["latin"] })
-
-export const metadata: Metadata = {
-  metadataBase: new URL("https://prayer-time-api.pages.dev"),
-  alternates: {
-    canonical: "/",
-  },
-  robots: {
-    index: true,
-    follow: true,
-  },
-  title: "Prayer Times API - Accurate Islamic Prayer Times for Developers",
-  description:
-    "Access reliable and precise Islamic prayer times for any location worldwide with our developer-friendly Prayer Times API.",
-}
-
-export const viewport = {
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-}
-
-interface RootLayoutProps {
+interface LayoutProps {
   children: ReactNode
 }
 
-export default function RootLayout({ children }: RootLayoutProps) {
+export default function Layout({ children }: LayoutProps) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="manifest" href="/manifest.json" />
-      </head>
-      <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <div className="flex flex-col min-h-screen">
-            <Header />
-            <main className="flex-grow">{children}</main>
-            <Footer />
-          </div>
-        </ThemeProvider>
-      </body>
-    </html>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow">{children}</main>
+      <Footer />
+    </div>
   )
 }
 
